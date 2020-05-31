cb75cafc35336ad50400e8cac35da4a189814599
Alexandr-Simich <simi55@mail.ru>
2020-05-31 20:28:59 +0300

    3.1.1

--- main.cpp
+++ main.cpp
@@ -1,30 +1,42 @@
 #include "histogram.h"
+#include <iostream>
 #include "svg.h"
 using namespace std;
 
-int main()
-{
-// 123
+vector<double> input_numbers(istream& in, size_t count);
+
+int main() {
+
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
 
+    if (number_count == 0) {
+        cerr << "ERROR: Empty vector";
+    } else {
+
     cerr << "Enter numbers: ";
-    const auto numbers = input_numbers(number_count);
+    const auto numbers = input_numbers(cin, number_count);
 
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
 
-    double min;
-    double max;
-    find_minmax(numbers,min,max);
+    double min, max;
+    find_minmax(numbers, min, max);
 
-    auto bins = make_histogram(numbers, bin_count, min, max);
-
-
-
+    const auto bins = make_histogram(numbers, bin_count, min, max);
     show_histogram_svg(bins, bin_count);
 
+    }
     return 0;
+}
+
+
+vector<double> input_numbers(istream& in, size_t count) {
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++) {
+        in >> result[i];
+    }
+    return result;
 }
