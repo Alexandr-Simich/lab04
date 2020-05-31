0db9fa721ccf78c6a8981fb34b25b783d1759c69
Alexandr-Simich <simi55@mail.ru>
2020-05-31 20:48:42 +0300

    3.1.2.3

--- main.cpp
+++ main.cpp
@@ -7,36 +7,47 @@ vector<double> input_numbers(istream& in, size_t count);
 
 
 
-Input read_input(istream& in) {
+Input read_input(istream& in, bool prompt)
+{
     Input data;
 
-    cerr << "Enter number count: ";
+    if(prompt)
+    {
+        cerr << "Enter number count: ";
+    }
     size_t number_count;
     in >> number_count;
 
-    cerr << "Enter numbers: ";
+    if(prompt)
+    {
+        cerr << "Enter numbers: ";
+    }
     data.numbers = input_numbers(in, number_count);
-
-    cerr << "Enter column count: ";
+    if(prompt)
+    {
+        cerr << "Enter column count: ";
+    }
     cin >> data.bin_count;
 
     return data;
 }
 
-int main() {
+int main()
+{
 
-    Input input = read_input(cin);
-
-    const auto bins = make_histogram(input);
+    Input input = read_input(cin, true);
+    vector<size_t> bins = make_histogram(input);
     show_histogram_svg(bins, input.bin_count);
 
     return 0;
 }
 
 
-vector<double> input_numbers(istream& in, size_t count) {
+vector<double> input_numbers(istream& in, size_t count)
+{
     vector<double> result(count);
-    for (size_t i = 0; i < count; i++) {
+    for (size_t i = 0; i < count; i++)
+    {
         in >> result[i];
     }
     return result;
